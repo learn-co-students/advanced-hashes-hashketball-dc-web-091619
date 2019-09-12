ff57c5e51e1de791f278e33a8a741a08877ce719
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-12 14:11:50 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -151,7 +151,11 @@ def shoe_size(players_name)
 end
 
 def team_colors(team_name)
-  
+  game_hash.each do |place, team|
+    if team[:team_name] == team_name
+      return team[:colors]
+    end
+  end
 end
 
 
